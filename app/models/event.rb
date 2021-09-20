d1acdf69c183c53f70120c019de0e2e63d675e01
adebadayo <adebayanen@gmail.com>
2021-09-21 00:50:29 +0900

    参加登録モーダル

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,7 @@ class Event < ApplicationRecord
   validate :start_at_should_be_before_end_at
 
   belongs_to :owner, class_name: "User"
+  has_many :tickets
 
   def created_by?(user)
     return false unless user
